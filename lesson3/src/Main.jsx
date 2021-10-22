2e8865b07e5fcfa20f3e2233cb473bf7faa26fe5
bleskova <bleskova.kate@gmail.com>
2021-10-22 16:26:39 +0300

    lesson3

---
+++ lesson3/src/Main.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import CustomInput from "./components/CustomInput/index";
+import List from "./components/List/index";
+import Task from "./components/Task/index";
+
+import './App.css';
+
+
+class Main extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: [],
+            total: 0,
+        }
+        this.addCart = this.addCart.bind(this)
+
+    }
+    componentDidMount() {
+        fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>{
+                this.setState({
+                    products: json
+                })
+            })
+    }
+    addCart(price){
+        const {products, total} = this.state;
+        this.setState({
+            total: price + total
+        })
+        console.log(products, price)
+    }
+
+    render() {
+        const {products, total} = this.state;
+        return (
+            <div>
+                <div className='cart'>
+                    <span>Total:</span>
+                    <span>{total.toFixed(2)}</span>
+                </div>
+                {products.length ? products.map((item) => <div key={item.id}>
+                    <h3>{item.title}</h3>
+                    <img className='image' src={item.image} alt=""/>
+                    <span>Price:</span>
+                    <span className='price'>{item.price}</span>
+                    <button onClick={() => this.addCart(item.price)}>Add to cart</button>
+                </div>)
+                : <p>Loading...</p>
+                }
+                
+            </div>
+        )
+    }
+}
+
+export default Main;
+
+// CustomInput.tsx       // Форма для добавления новой задачи
+//     Task.tsx        // Задача
+//     List.tsx        // Список задач
